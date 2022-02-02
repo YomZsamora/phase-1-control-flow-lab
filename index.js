f2d8dc77fde780fbdea0393d0a16b10bbe93ddf8
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 16:26:39 +0300

    commit tree 7f583e67b503096707e0809e98f88610ed363326

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+	// Write your code here!
+	if(distance <= 400) {
+		return 'This one is on me!';
+	}
+	else if(distance > 2000) {
+		if(distance > 2500) {
+			return 'No can do.';
+		}
+		return 'I will gladly take your thirty bucks.';
+	}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+	// Write your code here!
+	const checkCity = city == "NYC" ? "Ok, sounds good." : "No go.";
+	return checkCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+	// Write your code here!
+	let tipGiven;
+	switch(tip) {
+		case 'generous':
+		tipGiven = 'Thank you so much.';
+		break;
+		case 'not as generous':
+		tipGiven = 'Thank you.';
+		break;
+		case 'thanks for everything':
+		tipGiven = 'Bye.';
+		break;
+	}
+	return tipGiven;
 }
